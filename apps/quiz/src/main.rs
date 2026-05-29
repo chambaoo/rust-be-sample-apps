e4d96503208377b164b1fae4b989142b68699a01
chambaoo <chambao752@gmail.com>
2026-05-29 22:33:41 +0900

    add: new app quiz

---
+++ apps/quiz/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    println!("===== QUIZ =====");
+    println!("1 + 1 = ??");
+    println!("?? の値を入力してください:");
+
+    let mut ans_input = String::new();
+
+    std::io::stdin().read_line(&mut ans_input).unwrap();
+
+    let ans_input = ans_input.trim().parse::<u32>().unwrap();
+
+
+    dbg!(ans_input);
+
+    if dbg!(ans_input == 1 + 1) {
+        println!("正解");
+    } else {
+        println!("不正解");
+    }
+
+}
