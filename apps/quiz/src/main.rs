7ae8310bf85938ae75f49ebd6c57cc4715912ddd
chambaoo <chambao752@gmail.com>
2026-05-29 23:05:38 +0900

    update: branching for quiz mode

--- apps/quiz/src/main.rs
+++ apps/quiz/src/main.rs
@@ -4,10 +4,19 @@ fn main() {
     println!("===== QUIZ =====");
 
     let mut rng = rand::rng();
+    let quiz_mode = rng.random_range(1..=2);
     let op_left = rng.random_range(0..100);
     let op_right = rng.random_range(0..100);
 
-    println!("{} + {} = ??", op_left, op_right);
+    match quiz_mode {
+        1 => {
+            println!("{} + {} = ??", op_left, op_right);
+        }
+        2 => {
+            println!("{} - {} = ??", op_left, op_right);
+        }
+        _ => unreachable!(),
+    }
     println!("?? の値を入力してください:");
 
     let mut ans_input = String::new();
@@ -16,9 +25,21 @@ fn main() {
 
     let ans_input = ans_input.trim().parse::<u32>().unwrap();
 
-    if ans_input == op_left + op_right {
-        println!("正解");
-    } else {
-        println!("不正解");
+    match quiz_mode {
+        1 => {
+            if ans_input == op_left + op_right {
+                println!("正解");
+            } else {
+                println!("不正解");
+            }
+        }
+        2 => {
+            if ans_input == op_left - op_right {
+                println!("正解");
+            } else {
+                println!("不正解");
+            }
+        }
+        _ => unreachable!(),
     }
 }
