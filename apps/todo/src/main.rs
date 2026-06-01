c3a5b7d19696cee509b866c8895fb05bad3453e7
chambaoo <chambao752@gmail.com>
2026-06-01 19:28:06 +0900

    add: todo html template

--- apps/todo/src/main.rs
+++ apps/todo/src/main.rs
@@ -7,6 +7,24 @@ use askama_actix::TemplateToResponse;
 struct HelloTemplate {
     name: String,
 }
+
+#[derive(Template)]
+#[template(path = "todo.html")]
+struct TodoTemplate {
+    tasks: Vec<String>,
+}
+
+#[get("/")]
+async fn todo() -> HttpResponse {
+    let tasks = vec![
+        "task1".to_string(),
+        "task2".to_string(),
+        "task3".to_string(),
+    ];
+    let todo = TodoTemplate { tasks };
+    todo.to_response()
+}
+
 #[get("/hello")]
 async fn hello() -> String {
     "Hello.".to_string()
@@ -22,7 +40,7 @@ async fn hello_name(name: web::Path<String>) -> HttpResponse {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello).service(hello_name))
+    HttpServer::new(|| App::new().service(hello).service(hello_name).service(todo))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
