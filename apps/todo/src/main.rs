ee4a31d90755494d8388299b046bdb2081d22fcf
chambaoo <chambao752@gmail.com>
2026-06-01 18:17:21 +0900

    add: simple get method

--- apps/todo/src/main.rs
+++ apps/todo/src/main.rs
@@ -1,8 +1,13 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, get};
+
+#[get("/hello")]
+async fn hello() -> String {
+    "Hello.".to_string()
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new())
+    HttpServer::new(|| App::new().service(hello))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
