ec0deee7dff571b8edca30145a32667efe01ba09
chambaoo <chambao752@gmail.com>
2026-06-01 18:20:38 +0900

    update: get method with path parameter

--- apps/todo/src/main.rs
+++ apps/todo/src/main.rs
@@ -1,13 +1,19 @@
-use actix_web::{App, HttpServer, get};
+use actix_web::{App, HttpServer, get, web};
 
 #[get("/hello")]
 async fn hello() -> String {
     "Hello.".to_string()
 }
 
+#[get("/hello/{name}")]
+async fn hello_name(name: web::Path<String>) -> String {
+    // "Hello.".to_string()
+    format!("Hello, {name}.")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
+    HttpServer::new(|| App::new().service(hello).service(hello_name))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
