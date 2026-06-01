adbb98c902740f9eebd688e019a3819df18bfbab
chambaoo <chambao752@gmail.com>
2026-06-01 19:19:59 +0900

    add: html template

--- apps/todo/src/main.rs
+++ apps/todo/src/main.rs
@@ -1,14 +1,23 @@
-use actix_web::{App, HttpServer, get, web};
+use actix_web::{App, HttpResponse, HttpServer, get, web};
+use askama::Template;
+use askama_actix::TemplateToResponse;
 
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate {
+    name: String
+}
 #[get("/hello")]
 async fn hello() -> String {
     "Hello.".to_string()
 }
 
 #[get("/hello/{name}")]
-async fn hello_name(name: web::Path<String>) -> String {
-    // "Hello.".to_string()
-    format!("Hello, {name}.")
+async fn hello_name(name: web::Path<String>) -> HttpResponse {
+    let hello_name = HelloTemplate {
+        name: name.into_inner(),
+    };
+    hello_name.to_response()
 }
 
 #[actix_web::main]
@@ -17,5 +26,4 @@ async fn main() -> std::io::Result<()> {
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
-    
 }
